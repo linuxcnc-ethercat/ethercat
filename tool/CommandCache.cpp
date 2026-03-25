c5d90b0fd9ed735fd11ea5de71326744543e3c9b
tungnh30 <nguyenhoangtung1610@gmail.com>
2026-03-25 08:48:36 +0700

    Register cli for cache setting

---
+++ tool/CommandCache.cpp
@@ -0,0 +1,115 @@
+/*****************************************************************************
+ *
+ *
+ *  This file is part of the IgH EtherCAT Master.
+ *
+ *  The IgH EtherCAT Master is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License version 2, as
+ *  published by the Free Software Foundation.
+ *
+ *  The IgH EtherCAT Master is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ *  Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with the IgH EtherCAT Master; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ ****************************************************************************/
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+#include "CommandCache.h"
+#include "MasterDevice.h"
+
+/****************************************************************************/
+
+CommandCache::CommandCache():
+    Command("cache", "Configure SII caching settings.")
+{
+}
+
+/****************************************************************************/
+
+string CommandCache::helpString(const string &binaryBaseName) const
+{
+    stringstream str;
+
+    str << binaryBaseName << " " << getName() << " <FIELDS>" << endl
+        << endl
+        << getBriefDescription() << endl
+        << endl
+        << "This command configures which slave identity properties are" << endl
+        << "used for SII caching. SII caching allows the master to" << endl
+        << "identify slaves by properties other than their ring position," << endl
+        << "enabling slave communication even if the ring topology" << endl
+        << "changes." << endl
+        << endl
+        << "Caching fields are specified as a decimal number that is a" << endl
+        << "combination of the following flags:" << endl
+        << "  0 - Disable SII caching" << endl
+        << "  1 - Use vendor ID" << endl
+        << "  2 - Use product code" << endl
+        << "  4 - Use revision number" << endl
+        << "  8 - Use serial number" << endl
+        << " 16 - Use alias address" << endl
+        << endl
+        << "Example: 'ethercat cache 31' enables caching based on all" << endl
+        << "  five fields (1+2+4+8+16=31)." << endl
+        << "Example: 'ethercat cache 0' disables SII caching." << endl
+        << endl
+        << "Arguments:" << endl
+        << "  FIELDS  Decimal number representing the caching fields." << endl
+        << endl;
+
+    return str.str();
+}
+
+/****************************************************************************/
+
+void CommandCache::execute(const StringVector &args)
+{
+    MasterIndexList masterIndices;
+    uint32_t fields;
+    stringstream err;
+
+    if (args.size() != 1) 
+    {
+        err << "'" << getName() << "' requires exactly one argument!";
+        throwInvalidUsageException(err);
+    }
+
+    try {
+        fields = stoul(args[0]);
+    } catch (const exception &e) {
+        err << "Invalid caching fields value '" << args[0] << "': " << e.what();
+        throwInvalidUsageException(err);
+    }
+
+    if (fields > 31) {
+        err << "Caching fields value must be between 0 and 31.";
+        throwInvalidUsageException(err);
+    }
+
+    masterIndices = getMasterIndices();
+    MasterIndexList::const_iterator mi;
+    for (mi = masterIndices.begin(); mi != masterIndices.end(); mi++) {
+        MasterDevice m(*mi);
+        m.open(MasterDevice::ReadWrite);
+        
+        try {
+            m.setSiiCaching(fields);
+            cout << "Master " << dec << *mi << ": SII caching set to 0x"
+                << hex << setfill('0') << setw(2) << fields << dec << endl;
+        } catch (const MasterDeviceException &e) {
+            err << "Master " << *mi << ": " << e.what();
+            throwCommandException(err);
+        }
+    }
+}
+
+/****************************************************************************/
